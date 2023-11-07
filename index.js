39d894727279d6d7ed116272734a19be46387a20
farhanshahriyar <farhan.abir9999@gmail.com>
2023-11-07 23:14:14 +0600

    added jobs api and functionalities

--- index.js
+++ index.js
@@ -27,6 +27,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+     // Establish DB and verify connection
+     const jobCollection = client.db("jobsDB").collection("jobs");
+
+      // all post requests here
+      app.post('/jobs', async (req, res) => {
+        const newJob = req.body;
+        // console.log(newJob); // checking if the data is coming or hitting (ok)
+        const result = await jobCollection.insertOne(newJob);
+        res.json(result);
+      });
+
+    // all get requests here
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
